e05bd704983c733b9561d43aa31f1005dc95d982
Marc-Oliver Pahl <pahl@net.in.tum.de>
2011-12-30 19:56:20 +0000

    Fixed menu.inc to work when IS_CONFIG_EDITOR is not defined.

--- plugins/LSE/TestLabSystemExportEPub.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('LabSystemExportEPub.php');
-
-$epub = LabSystemExportEPub::getInstance();
-$epub->save('p', '<p>Something is here</p>', array('par1' => 'val1'));
-$epub->save('c', 'This is a place holder for c', array('par2' => 'val2'));
-
-
