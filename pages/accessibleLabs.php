cc5250bbb9e05d4383bd651a7982080264a6b83e
Marc-Oliver Pahl <pahl@net.in.tum.de>
2012-02-28 22:54:36 +0000

    /pages/accessableLabs.php     Renamed the misspelled file to accessIbleLabs.php.     Added code for the preface.   /ini/config_*.ini     Added prefixID as variable to set the prefix for the ePub.

--- pages/accessibleLabs.php
+++ pages/accessibleLabs.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  labsystem.m-o-p.de - 
+ *  labsystem.m-o-p.de -
  *                  the web based eLearning tool for practical exercises
  *  Copyright (C) 2011  Marc-Oliver Pahl
  *
@@ -19,63 +19,76 @@
  */
 
 /**
-* Edit page for the main menu ini-file.
-*
-* @module     ../pages/accessibleLabs.php
-* @author     Marc-Oliver Pahl
-*/
+ * Edit page for the main menu ini-file.
+ *
+ * @module     ../pages/accessibleLabs.php
+ * @author     Marc-Oliver Pahl
+ */
 require( "../include/init.inc" );
 
 $pge->matchingMenu = $lng->get('MnuEntryCourseContent');
 $pge->title        = $cfg->get('SystemTitle').' '.$lng->get('MnuEntryCourseContent');
-$returnEpub = $url->available( 'ePub' );
+$returnEpub        = $url->available( 'ePub' );
 
-  // head (create new)
-     if ( !$returnEpub && $usr->isOfKind( IS_CONTENT_EDITOR ) ) $pge->put(  "<div class=\"labsys_mop_elements_menu_p\">\n".
-           EB::mkLink(  $url->link2( '../pages/accessibleLabs.php?ePub=ePub' ),
+// head (create new)
+if ( !$returnEpub && $usr->isOfKind( IS_CONTENT_EDITOR ) ) $pge->put(  "<div class=\"labsys_mop_elements_menu_p\">\n".
+EB::mkLink(  $url->link2( '../pages/accessibleLabs.php?ePub=ePub' ),
                         "<img src=\"../syspix/button_epub_12x12.gif\" width=\"12\" height=\"12\" border=\"0\" alt=\"link to\" title=\"".$lng->get("explainLink2epub")."\">" ).
-           EB::link2Url( '../pages/accessibleLabs.php' )."</div>\n" );
-     if ($returnEpub){
-       echo("initializing ePub<br>");
-       //TODO: Call functions to tell ePub export that multiple labs come now.
-       echo("Adding foreword page...<br>");
-       //TODO: Add forword.
-     }
-  // title
-     if (!$returnEpub){$pge->put( "<div class=\"labsys_mop_h2\">__PAGETITLE__</div>\n" );}
-     
-  // note
-     if ( !$returnEpub && $lng->get("AccessibleLabsNote") != "" ) $pge->put( "<div class=\"labsys_mop_note\">\n".$lng->get("AccessibleLabsNote")."</div>\n" ); 
-
-     $accessibleLabs = array();
-     $alreadyAdded = array(); // for not adding dups...
-  // Labs that are visible without a schedule
-    //SELECT `idx` FROM `labs` WHERE `visible_before_first_sched`=1
-    $lDBI->queryResult = $lDBI->myDBC->mkSelect( '*', $lDBI->myTable, "idx!=1 && `visible_before_first_sched`=1" );
-    while( $nextElement=$lDBI->getNextData() ){
-      $accessibleLabs[] = $nextElement;
-      $alreadyAdded[] = $nextElement->idx;
+EB::link2Url( '../pages/accessibleLabs.php' )."</div>\n" );
+if ($returnEpub){
+  echo("initializing ePub<br>");
+  //TODO: Call functions to tell ePub export that multiple labs come now.
+  if ($cfg->doesExist('prefaceID')){
+    $prefaceID = $cfg->get("prefaceID");
+    $id = $prefaceID{0}; $num = substr( $prefaceID, 1);
+    require( "../php/getDBIbyID.inc" ); /* -> $DBI */
+    if ( !$preface = $DBI->getData2idx( $num ) ){
+      trigger_error( $lng->get(strtolower( $id )."Number").$num." ".$lng->get("doesNotExist"), E_USER_ERROR );
+      exit;
     }
+    echo("Adding foreword page: "+$preface->title+"<br>");
+    //TODO: Add forword to ePub: $preface->showEPub( $prefaceID, '' );
+  }
+}
+// title
+if (!$returnEpub){
+  $pge->put( "<div class=\"labsys_mop_h2\">__PAGETITLE__</div>\n" );
+}
 
-  // Currently scheduled in order of schedule
-    //SELECT `num` FROM `schedules` WHERE 1 GROUP BY num ORDER BY `start`
-    $sDBI->queryResult = $sDBI->myDBC->mkSelect( 'num', $sDBI->myTable, 'idx!=1', '`start`', 'num' );
-    while( $nextData=mysql_fetch_array($sDBI->queryResult) ){
-      if (  !in_array( $nextData['num'], $alreadyAdded ) && ($nextLab = $lDBI->getData2idx( $nextData['num'] )) ) // do not add twice...
-        $accessibleLabs[] = $nextLab;
-    }
+// note
+if ( !$returnEpub && $lng->get("AccessibleLabsNote") != "" ) $pge->put( "<div class=\"labsys_mop_note\">\n".$lng->get("AccessibleLabsNote")."</div>\n" );
 
-     $counter = 0;
-     $charCounter = 97;
-     if (!$returnEpub){$pge->put('<table align="center" width="80%" cellspacing="10">');}
-     foreach ( $accessibleLabs as $value ){
-       if ($returnEpub){
-         $extParagraph = (string)( $value->visibleBefore1stSched ? chr ($charCounter++) : $counter++ );
-         echo( $extParagraph.' '.$value->title.' ('.$value->elementId.$value->idx.')<br>');
-         //echo( $value->showTOC( $value->elementId.$value->idx, $extParagraph ) );
-         // to be done: $value->showEPub( $value->elementId.$value->idx, ( $value->visibleBefore1stSched ? chr ($charCounter++) : $counter++ ) );
-       }else{
-         $pge->put('
+$accessibleLabs = array();
+$alreadyAdded = array(); // for not adding dups...
+// Labs that are visible without a schedule
+//SELECT `idx` FROM `labs` WHERE `visible_before_first_sched`=1
+$lDBI->queryResult = $lDBI->myDBC->mkSelect( '*', $lDBI->myTable, "idx!=1 && `visible_before_first_sched`=1" );
+while( $nextElement=$lDBI->getNextData() ){
+  $accessibleLabs[] = $nextElement;
+  $alreadyAdded[] = $nextElement->idx;
+}
+
+// Currently scheduled in order of schedule
+//SELECT `num` FROM `schedules` WHERE 1 GROUP BY num ORDER BY `start`
+$sDBI->queryResult = $sDBI->myDBC->mkSelect( 'num', $sDBI->myTable, 'idx!=1', '`start`', 'num' );
+while( $nextData=mysql_fetch_array($sDBI->queryResult) ){
+  if (  !in_array( $nextData['num'], $alreadyAdded ) && ($nextLab = $lDBI->getData2idx( $nextData['num'] )) ) // do not add twice...
+  $accessibleLabs[] = $nextLab;
+}
+
+$counter = 0;
+$charCounter = 97;
+if (!$returnEpub){
+  $pge->put('<table align="center" width="80%" cellspacing="10">');
+}
+foreach ( $accessibleLabs as $value ){
+  if ($returnEpub){
+    $extParagraph = (string)( $value->visibleBefore1stSched ? chr ($charCounter++) : $counter++ );
+    echo( $extParagraph.' '.$value->title.' ('.$value->elementId.$value->idx.')<br>');
+    //echo( $value->showTOC( $value->elementId.$value->idx, $extParagraph ) );
+    // to be done: $value->showEPub( $value->elementId.$value->idx, ( $value->visibleBefore1stSched ? chr ($charCounter++) : $counter++ ) );
+  }else{
+    $pge->put('
 <tr>
   <td width="75" class="labIndexNumber">
 '.( $value->visibleBefore1stSched ? chr ($charCounter++) : $counter++ ).'
@@ -84,12 +97,16 @@ $returnEpub = $url->available( 'ePub' );
     <b><a href="../pages/view.php?address=l'.$value->idx.'&amp;__LINKQUERY__" target="_top">'.$value->title.'</a></b> - '.$value->comment.'
     </td>
   </tr>
-'        );
-       }
-     }
-     if (!$returnEpub){$pge->put('</table>');}
-  
-if (!$returnEpub){require( $cfg->get("SystemPageLayoutFile") );}
+');
+  }
+}
+if (!$returnEpub){
+  $pge->put('</table>');
+}
+
+if (!$returnEpub){
+  require( $cfg->get("SystemPageLayoutFile") );
+}
 else{
   echo("creating ePub");
 }
