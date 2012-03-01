f8bf892023e66c736017faba9019206088436c8c
Marc-Oliver Pahl <pahl@net.in.tum.de>
2012-03-01 23:23:44 +0000

    /pages/mailForm.php   /php/sendMail.php -> /php/sendMail.inc     Changed the mailForm so that it keeps the message content when sending.     This is very helpful when one wants to send information with slight changes to several users (like marks).

--- pages/mailForm.php
+++ pages/mailForm.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  labsystem.m-o-p.de - 
+ *  labsystem.m-o-p.de -
  *                  the web based eLearning tool for practical exercises
  *  Copyright (C) 2010  Marc-Oliver Pahl
  *
@@ -23,10 +23,12 @@
 *
 * @module     ../pages/mailForm.php
 * @author     Marc-Oliver Pahl
-* @copyright  Marc-Oliver Pahl 2005
-* @version    1.0
 */
 require( "../include/init.inc" );
+
+if (isset($_POST['SESSION_ID'])){
+  require("../php/sendMail.inc");
+}
 
 $pge->title       = $lng->get("titleSendMail");
 $pge->matchingMenu= "send mail";
@@ -55,7 +57,7 @@ $content = "
 function setCheckboxes(do_check)
 {
   var i = 0;
-  while (typeof(document.MailForm.elements[i]) != 'undefined') 
+  while (typeof(document.MailForm.elements[i]) != 'undefined')
     if ( document.MailForm.elements[i].type == 'checkbox' && document.MailForm.elements[i].name != 'NO_COPY_2_ME')
       document.MailForm.elements[i++].checked = do_check;
     else i++;
@@ -74,7 +76,7 @@ $urDBI->getAllData();
 // iterate over all users that have rights
 while( $userRightsData = $urDBI->getNextData() ){
   $userData = $uDBI->getData4( $userRightsData["uid"] );
- // create the user element 
+ // create the user element
   $user = new ElementUser( $userData["uid"], $userData["userName"], $userData["foreName"], $userData["name"], $userRightsData["currentTeam"], $userRightsData["rights"], $userData["eMail"], $userRightsData["history"] );
  // ignore user that have no mailaddress or are no IS_MAIL_RECEIVER
   if ( !$user->isOfKind( IS_MAIL_RECEIVER ) || !$userData || ($userData["eMail"] == "") ) continue;
@@ -109,14 +111,14 @@ if ( $usr->isOfKind( IS_ALL_MAILER ) )
   foreach( $allOther as $key => $value ) $allOtherInputs         .= "<input tabindex=\"".$pge->nextTab++."\" type=\"checkbox\" id=\"MAIL2_".++$counter."\" name=\"MAIL2_".$counter."\" value='".$value."'".( $checkAll ?  " checked=\"checked\" "  : '' )." onchange='isDirty=true'>".
                                                                     "<label for=\"MAIL2_".$counter."\" class=\"labsys_mop_input_field_label\">".$key."</label><br />\n";
 
-$content .= "<FORM class=\"labsys_mop_std_form\" NAME=\"MailForm\" METHOD=\"POST\" ACTION=\"".$url->link2("../php/sendMail.php")."\">\n".
+$content .= "<FORM class=\"labsys_mop_std_form\" NAME=\"MailForm\" METHOD=\"POST\" ACTION=\"".$url->link2("../pages/mailForm.php")."\">\n".
             "<input type=\"hidden\" name=\"SESSION_ID\" value=\"".session_id()."\">\n".
             "<input type=\"hidden\" name=\"REDIRECTTO\" value=\"../pages/mailForm.php\">\n". /* index of saved el. will be added on save.php! */
             "<input type=\"hidden\" name=\"POSSIBLE_RECVR\" value=\"".$counter."\">\n".
             "<table class=\"labsys_mop_mailform_table\">\n".
             "<tr><td class=\"labsys_mop_mailform_table_mail2\"></td><td class=\"labsys_mop_mailform_table_the_mail\"></td></tr>\n".
 
-           // the mail2 row 
+           // the mail2 row
             "<tr><td class=\"labsys_mop_mailform_table_mail2\">\n".
 
             ( $usr->isOfKind( IS_ALL_MAILER ) ?
@@ -125,40 +127,41 @@ $content .= "<FORM class=\"labsys_mop_std_form\" NAME=\"MailForm\" METHOD=\"POST
                         "<a href=\"".$url->link2("../pages/mailForm.php")."\" onclick=\"setCheckboxes(false); return false;\">".$lng->get("unCheckAll")."</a><br />\n".
                         "</fieldset>\n" : ''
                       ).
-            
+
             "<fieldset><legend>".$lng->get("labSupporter")."</legend>\n".
             $allSupporterInputs.
             "</fieldset>\n".
-            
+
             ( $usr->isOfKind( IS_ALL_MAILER )?
                         "<fieldset><legend>".$lng->get("otherUser")."</legend>\n".
                         $allOtherInputs.
                         "</fieldset>\n" : ''
                       ).
-            
+
             "</td>".
 
            // the mail subject/ body side
             "<td class=\"labsys_mop_mailform_table_the_mail\">\n".
-            
+
             "<fieldset><legend>".$lng->get("yourMail")."</legend>\n".
            // subject
             "<label for=\"subject\" class=\"labsys_mop_input_field_label_top\">".$lng->get("subject")."</label>".
-            "<input tabindex=\"".$pge->nextTab++."\" type=\"text\" id=\"subject\" name=\"SUBJECT\" class=\"labsys_mop_input_fullwidth\" maxlength=\"255\" value=\"\" onchange='isDirty=true'>\n".
+            "<input tabindex=\"".$pge->nextTab++."\" type=\"text\" id=\"subject\" name=\"SUBJECT\" class=\"labsys_mop_input_fullwidth\" maxlength=\"255\" value=\"".(isset($_POST['SUBJECT']) ? $_POST['SUBJECT'] : '')."\" onchange='isDirty=true'>\n".
            // mailbody
             "<label for=\"mailtext\" class=\"labsys_mop_input_field_label_top\">".$lng->get("message")."</label>".
             "<textarea tabindex=\"".$pge->nextTab++."\" id=\"mailtext\" name=\"MAILTEXT\" class=\"labsys_mop_textarea\" rows=\"".$cfg->get("sendMailBodyRows")."\" onchange='isDirty=true'>".
+            (isset($_POST['MAILTEXT']) ? $_POST['MAILTEXT'] : '').
             "</textarea>\n".
             "</fieldset>\n".
-            
+
             "<input tabindex=\"".$pge->nextTab++."\" type=\"checkbox\" id=\"noCopy2Me\" name=\"NO_COPY_2_ME\" value=\"1\" checked=\"checked\" onchange='isDirty=true'>".
             "<label for=\"noCopy2Me\" class=\"labsys_mop_input_field_label\">".$lng->get("noCopy2Me")."</label><br />\r\n".
 
             "<input tabindex=\"".$pge->nextTab++."\" type=\"checkbox\" id=\"mailViaBcc\" name=\"MAIL_VIA_BCC\" value=\"1\"".( $cfg->get("mailViaBcc") == '1' ?  " checked=\"checked\""  : '' )." onchange='isDirty=true'>".
             "<label for=\"mailViaBcc\" class=\"labsys_mop_input_field_label\">".$lng->get("mailViaBcc")."</label>".
 
-            "<input tabindex=\"".$pge->nextTab++."\" type=\"submit\" class=\"labsys_mop_button_fullwidth\" value=\"".$lng->get("sendMail")."\" accesskey=\"s\" onclick='isDirty=false'>\n".            
-                        
+            "<input tabindex=\"".$pge->nextTab++."\" type=\"submit\" class=\"labsys_mop_button_fullwidth\" value=\"".$lng->get("sendMail")."\" accesskey=\"s\" onclick='isDirty=false'>\n".
+
             "</td></tr>\n".
             "</table>\n".
 
