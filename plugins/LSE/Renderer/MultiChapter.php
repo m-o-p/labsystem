efd3fb01e9e72ec5df8b49728001dec7e57abea8
Marc-Oliver Pahl <pahl@net.in.tum.de>
2012-04-15 13:31:44 +0000

    LSE Update by Bibek:
    labsys_epub_theme.css
    LpElement.inc
    MultiChapter.php
    BigC.phtml
    Ll.phtml

--- plugins/LSE/Renderer/MultiChapter.php
+++ plugins/LSE/Renderer/MultiChapter.php
@@ -7,14 +7,14 @@ class LSE_Renderer_MultiChapter implements LSE_Renderer_Interface
     protected $_log;
     protected $_engine;
     protected $_plugin;
-    
+
     public function __construct(LSE_Engine $engine)
     {
         $this->_log = new LSE_Logger('LSE_Renderer_MultiChapter');
         $this->_engine = $engine;
         $this->_plugin = $engine->getEpub();
     }
-    
+
     public function render()
     {
         $this->_setupCoverImage();
@@ -23,7 +23,7 @@ class LSE_Renderer_MultiChapter implements LSE_Renderer_Interface
         $this->_setupChapters();
         $this->_finalize();
     }
-    
+
     protected function _setupCoverImage()
     {
         $coverImage = $this->_engine->getBook()->getCoverImage();
@@ -40,45 +40,45 @@ class LSE_Renderer_MultiChapter implements LSE_Renderer_Interface
             unlink($dstPath);
         }
     }
-    
+
     protected function _setupMultiChapterTOC()
     {
         $book = $this->_engine->getBook();
         $graph = $book->buildGraph(array("l"));
         $elementTable = $book->getElementTable();
-        
+
         // Add a TOC in each chapter in graph
-        foreach ($graph as $key => $lElement) {
-            $graph[$key] = array("toc" => array()) + $lElement;
-        }
+//        foreach ($graph as $key => $lElement) {
+//            $graph[$key] = array("toc" => array()) + $lElement;
+//        }
         $elementTable += array('toc' => array('toc', 'Table of Contents'));
-        
+
         $graphPrefix = array();
         $elementTablePrefix = array();
-        
+
         $graphPrefix['multiChapterTocPage'] = array();
         $elementTablePrefix['multiChapterTocPage'] = array('multiChapterTocPage', 'Table of Contents');
-        
+
         if ($book->getPreface()) {
             $graphPrefix['preface'] = array();
             $elementTablePrefix['preface'] = array('preface', 'Preface');
         }
         $graph = $graphPrefix + $graph;
         $elementTable = $elementTablePrefix + $elementTable;
-        
+
 //        $this->_log->log($graph);
 //        $this->_log->log($elementTable);
-        
+
         $this->_plugin->setNcxFromGraph($graph, $elementTable);
-        
+
         $view = new SPT_View();
         $view->assign(array('graph' => $graph, 'elementTable' => $elementTable));
         $tocChapter = $view->render(LSE_ROOT . '/templates/multichapter/toc.phtml', true);
-        
-        $this->_plugin->addChapter( 'multiChapterTocPage', 'multiChapterTocPage.html', $tocChapter, FALSE, 
+
+        $this->_plugin->addChapter( 'multiChapterTocPage', 'multiChapterTocPage.html', $tocChapter, FALSE,
             EPub::EXTERNAL_REF_ADD);
     }
-    
+
     protected function _setupPreface()
     {
         $preface = $this->_engine->getBook()->getPreface();
@@ -89,7 +89,7 @@ class LSE_Renderer_MultiChapter implements LSE_Renderer_Interface
             $this->_plugin->addChapter( 'preface', 'preface.html', $prefaceContent, FALSE, EPub::EXTERNAL_REF_ADD);
         }
     }
-    
+
     protected function _setupChapters()
     {
         $book = $this->_engine->getBook();
@@ -97,7 +97,7 @@ class LSE_Renderer_MultiChapter implements LSE_Renderer_Interface
         $output = '';
         foreach ($chapters as $chapterId => $chapter) {
             $output = $book->renderChapter($chapterId);
-            
+
             if (LSE_DEBUG) {
                 print $output;
             }
@@ -106,14 +106,14 @@ class LSE_Renderer_MultiChapter implements LSE_Renderer_Interface
             }
         }
     }
-    
+
     protected function _finalize()
     {
         $isFinalized = $this->_plugin->finalize();
         $this->_log->log($isFinalized, 'isFinalized');
 
         $bookTitle = str_replace(' ', '_', strtolower($this->_engine->getBook()->getTitle()));
-            
+
             // bookTitle is usually htmlencoded, so decode this first
         $bookTitle = LSE_Util::filterPTag($bookTitle);
         if (!LSE_DEBUG) {
