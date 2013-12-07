78b5e0f649c3e34bb8fcfa0c36afba10e5fdb0a9
Marc-Oliver Pahl <pahl@net.in.tum.de>
2013-12-07 18:05:34 +0000

    Removed debug output when creating an element in a collection with the new mechanism. Changed the prototype in the setup to be empty.

--- setup/sql_new_ddb_collections_tbl.sql
+++ setup/sql_new_ddb_collections_tbl.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `collections` (
 ) AUTO_INCREMENT=7 ;
 
 INSERT INTO `collections` (`idx`, `title`, `contents`, `matching_menu`, `visible_only_in_collection`, `visible_before_first_sched`, `visible_during_sched`, `visible_after_first_sched`, `history`) VALUES
-(1, 'prototype of a collection element', 'p1 i1 m1', '', 1, 0, 0, 0, CONCAT( now(), ': Marc-Oliver Pahl' )),
+(1, 'prototype of a collection element', '', '', 1, 0, 0, 0, CONCAT( now(), ': Marc-Oliver Pahl' )),
 (2, 'Demonstration PreLab content', 'p4 m2 p8', '', 1, 0, 0, 0, CONCAT( now(), ': Marc-Oliver Pahl' )),
 (3, 'Demonstration Lab content...', 'p7 i2', '', 1, 0, 0, 0, CONCAT( now(), ': Marc-Oliver Pahl' )),
 (4, 'Suggestions/ complaints', 'p9 i3 p12', '', 1, 0, 0, 0, CONCAT( now(), ': Marc-Oliver Pahl' )),
