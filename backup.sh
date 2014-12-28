d734aa7c2efa1acbcc99d5a71e7468147121df8a
Marc-Oliver Pahl <pahl@net.in.tum.de>
2014-12-28 15:57:50 +0000

    Thank you Andreas Korsten for making the backup script ready for running as cron job! Simply include a symlink into cron daily and you have backups in the backup directory.

--- backup.sh
+++ backup.sh
@@ -2,17 +2,31 @@
 #
 # Add this to CRON if you want n daily backups for instance.
 #
+
+BASENAME="$(readlink -f "$0")"
+BASEDIR="$(dirname "$BASENAME")"
+BASENAME="$(basename "$BASENAME")"
+
 if ! type php5 >/dev/null 2>&1 ; then
 	echo "No comman line interface for php5 found."
 	exit 1
 fi
-cd "$( readlink -f "$( dirname "$0" )" )"
-(
-read directory || echo "The first line of backup.txt must contain the backup directory." >&2
-while read nextInstance ; do
-	if [ -n "$nextInstance" ] ; then
-		echo "Backing instance $nextInstance up:"
-		( cd ./php/ && php5 cliDoInstanceBackup.php "$nextInstance" "$directory" )
-	fi
+
+mapfile -t -u3 3<"${BASEDIR}/backup.txt"
+if ! [ -n "${MAPFILE[0]}" -a -d  "${MAPFILE[0]}" ]; then
+	echo  "The first line of backup.txt must contain the backup directory." >&2
+	exit 1
+fi
+backupdir="${MAPFILE[0]}"
+for ((i=1;i<${#MAPFILE[@]};i++)); do
+	[ -n "${MAPFILE[i]}" ] || continue
+	echo "Backing instance ${MAPFILE[i]} up:" >&2
+	(
+		if cd "${BASEDIR}/php"; then
+			php5 "cliDoInstanceBackup.php" \
+				"${MAPFILE[i]}" "$backupdir"
+		else
+			echo "ERROR: could not cd to ${BASEDIR}/php"
+		fi
+	)
 done
-)<backup.txt
