02cddcea25d3a5939d18845b0b391b9aec648f15
Marc-Oliver Pahl <pahl@net.in.tum.de>
2015-09-23 19:23:09 +0200

    Fixed Bug with password encryption function for admin password on initial setup. Removed /include/init.inc password.inc as dependency (PHP5.5.5.).

--- setup/index.php
+++ setup/index.php
@@ -240,7 +240,7 @@ if(!$usrDB->table_exists( $cfg->get("UserDatabaseTable") ))
                    '.$cfg->get("User_courseID").'
                    ) VALUES (
                    \''.$cfg->get("RightsAdminUsername").'\',
-                   \''.password_hash( 'admin', md5( $cfg->get("RightsAdminUsername") ) ).'\',
+                   \''.crypt( 'admin', md5( $cfg->get("RightsAdminUsername") ) ).'\',
                    \'admin\',
                    \'Ms/ Mr\',
                    \'root@localhost\',
