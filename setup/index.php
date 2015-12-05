da16e0629af73a2d9685a9a987e227689deaad89
Marc-Oliver Pahl <pahl@net.in.tum.de>
2015-12-05 13:38:40 +0100

    Changed the password hashing algorithm to the new appropriate one in the setup script so that admin login works as announced.

--- setup/index.php
+++ setup/index.php
@@ -240,7 +240,7 @@ if(!$usrDB->table_exists( $cfg->get("UserDatabaseTable") ))
                    '.$cfg->get("User_courseID").'
                    ) VALUES (
                    \''.$cfg->get("RightsAdminUsername").'\',
-                   \''.crypt( 'admin', md5( $cfg->get("RightsAdminUsername") ) ).'\',
+                   \''.password_hash( 'admin', PASSWORD_DEFAULT ).'\',
                    \'admin\',
                    \'Ms/ Mr\',
                    \'root@localhost\',
