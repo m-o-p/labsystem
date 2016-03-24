7e45c3ce124c18ccea6cdc8dc90a95015674254d
Marc-Oliver Pahl <pahl@net.in.tum.de>
2016-03-24 15:41:07 +0100

    Changed the password functoinalilty towards token-based. Put functionality into the pages file.

--- pages/uaUnPwReminder.php
+++ pages/uaUnPwReminder.php
@@ -19,58 +19,111 @@
  */
 
 /**
-* This page is part of the UserAdministration configuration and enables you to
-* GET A REMINDER EMAIL containing UserName and PassWord.
-*
-* @module     ../pages/uaUnPwRemainder.php
-* @author     Marc-Oliver Pahl
-* @copyright  Marc-Oliver Pahl 2005
-* @version    1.0
-*/
-require( "../include/init.inc" );
+ * This page is part of the UserAdministration configuration and enables you to
+ * GET A REMINDER EMAIL containing UserName and PassWord.
+ *
+ * @module ../pages/uaUnPwRemainder.php
+ *
+ * @author Marc-Oliver Pahl
+ * @copyright Marc-Oliver Pahl 2016
+ * @version 1.1
+ *         
+ */
+require ("../include/init.inc");
 
-$pge->title        = $lng->get("titleUaUnPwRem");
-$pge->matchingMenu = $lng->get("MnuEntryUaUnPwRem");
-$pge->visibleFor   = IS_GUEST;
+$pge->title = $lng->get ( "titleUaUnPwRem" );
+$pge->matchingMenu = $lng->get ( "MnuEntryUaUnPwRem" );
+$pge->visibleFor = IS_GUEST;
 
-  if ( substr( $url->get('config'), -9 ) != 'useradmin' ) $pge->put( "<div class=\"labsys_mop_note\">\n".$lng->get("TxtNotConfigUA")."\n</div>" );
-  else{ // showing password fields
-     // title
-     $pge->put( "<div class=\"labsys_mop_h2\">__PAGETITLE__</div>\n" );
-     
-     // note
-     if ( $lng->get("uaUnPwRemNote") != "" ) $pge->put( "<div class=\"labsys_mop_note\">\n".$lng->get("uaUnPwRemNote")."</div>\n" );  
-      
-     $pge->put( "<FORM class=\"labsys_mop_std_form\" NAME=\"UnPwRemainder\" METHOD=\"POST\" ACTION=\"".$url->link2("../php/uaUnPwRemind.php")."\">\n".
-                "<input type=\"hidden\" name=\"REDIRECTTO\" value=\"../pages/uaUnPwReminder.php\">\n".
-                "<fieldset><legend>".$lng->get("eMail")."</legend>\n".
-                "<div class=\"labsys_mop_in_fieldset\">\n" );
+// Something to process?
+if (isset ( $_POST ['EMAIL'] ) || isset ( $_GET ['EMAIL'] )) {
+	$requesterEmail = (isset ( $_POST ['EMAIL'] ) ? $_POST ['EMAIL'] : $_GET ['EMAIL']);
+	// Load data to mail address
+	// new Interface to the userDB
+	$userDBC = new DBConnection ( $cfg->get ( 'UserDatabaseHost' ), $cfg->get ( 'UserDatabaseUserName' ), $cfg->get ( 'UserDatabasePassWord' ), $cfg->get ( 'UserDatabaseName' ) );
+	
+	// check if the mailAddress exists:
+	$result = $userDBC->mkSelect ( 'pwReminderToken,pwReminderValidUntil,' . $cfg->get ( 'UserDBField_uid' ) . ' AS uid', $cfg->get ( 'UserDatabaseTable' ), 'UPPER(' . $cfg->get ( 'UserDBField_email' ) . ")=UPPER('" . $requesterEmail . "')" );
+	if (! isset ( $SYSALERT )) {
+		$SYSALERT = '';
+	}
+	if (mysql_num_rows ( $result ) < 1)
+		// alert
+		$SYSALERT .= $requesterEmail . ' ' . $lng->get ( 'uaNotBelong2Usr' );
+	else {
+		$mailPage = $GLOBALS ["pDBI"]->getData2idx ( $cfg->get ( 'PidPasswordMail' ) );
+		while ( $data = mysql_fetch_assoc ( $result ) ) {
+			if (isset ( $_GET ['TOKEN'] ) && ($data ['pwReminderToken'] == $_GET ['TOKEN']) && ($data ['pwReminderValidUntil'] - time () > 0)) {
+				// Token fits and is still valid
+				// generate new Password:
+				srand ( ( double ) microtime () * 1000000 );
+				$newPW = substr ( sha1 ( uniqid ( rand () ) ), 13, 10 );
+				
+				// set the new password, reset token data
+				$userDBC->mkUpdate ( 'pwReminderToken=DEFAULT, pwReminderValidUntil=DEFAULT, ' . $cfg->get ( 'UserDBField_password' ) . "='" . password_hash ( $newPW, PASSWORD_DEFAULT ) . "'", $cfg->get ( 'UserDatabaseTable' ), $cfg->get ( 'UserDBField_uid' ) . "='" . $data [$cfg->get ( 'UserDBField_uid' )] . "'" );
+				
+				// find out where the user can log in:
+				$groupMemberships = '';
+				$result = $userDBC->mkSelect ( '*', $cfg->get ( 'UserDatabaseTable' ), $cfg->get ( 'UserDBField_uid' ) . '="' . $data ['uid'] . '"' );
+				while ( $memberData = mysql_fetch_assoc ( $result ) ) {
+					foreach ( $memberData as $key => $value ) {
+						if ($key [0] == '_' && $value == 1) {
+							$groupMemberships .= substr ( $key, 1 ) . PHP_EOL;
+						}
+					}
+				}
+				$mailPage->contents .= $lng->get ( 'passWord' ) . ': ' . $newPW . PHP_EOL . PHP_EOL . $lng->get ( 'YouAreMemberOf' ) . ': ' . PHP_EOL . $groupMemberships;
+			} else {
+				// Store a new token and validity for 1 hour
+				$token = sha1 ( uniqid ( rand () ) );
+				$userDBC->mkUpdate ( 'pwReminderToken="' . $token . '", pwReminderValidUntil=' . (time () + 60 * 60), $cfg->get ( 'UserDatabaseTable' ), $cfg->get ( 'UserDBField_uid' ) . "='" . $data [$cfg->get ( 'UserDBField_uid' )] . "'" );
+				$urlParts = parse_url ( 'http' . (isset ( $_SERVER ['HTTPS'] ) ? 's' : '') . '://' . $_SERVER ['HTTP_HOST'] . $_SERVER ['REQUEST_URI'] );
+				
+				$mailPage->contents .= 'https://' . $urlParts ['host'] . $urlParts ['path'] . '?config=useradmin&EMAIL=' . urlencode ( $requesterEmail ) . '&TOKEN=' . urlencode ( $token );
+			}
+			require_once (INCLUDE_DIR . "/classes/MailFunctionality.inc");
+			$mailFunc->sendMail ( '', $data ['uid'], $mailPage->title, $mailPage->contents );
+			$SYSALERT .= $lng->get ( 'MailHasBeenSent' ) . ' ' . $cfg->get ( 'SystemTitle' );
+		}
+	}
+}
 
-     $pge->put( 
-     // email address
-                '<label for="eMail" class="labsys_mop_input_field_label_top">'.$lng->get('eMail').'</label>'."\n".
-                '<input tabindex="'.$pge->nextTab++.'" type="text" id="eMail" name="EMAIL" class="labsys_mop_input_fullwidth" value="user@server.tld" />'."\n"
-               );
-               
-     $pge->put( "</div>\n".
-                "</fieldset>\n".
-                "<input tabindex=\"".$pge->nextTab++."\" type=\"submit\" class=\"labsys_mop_button\">\n".            
-                "</FORM>"
-               );
-               
-// focus
-     $pge->put(
-                '<script language="JavaScript" type="text/javascript">
+if (substr ( $url->get ( 'config' ), - 9 ) != 'useradmin')
+	$pge->put ( "<div class=\"labsys_mop_note\">\n" . $lng->get ( "TxtNotConfigUA" ) . "\n</div>" );
+else { // showing password fields
+       // title
+	$pge->put ( "<div class=\"labsys_mop_h2\">__PAGETITLE__</div>\n" );
+	
+	// note
+	if ($lng->get ( "uaUnPwRemNote" ) != "")
+		$pge->put ( "<div class=\"labsys_mop_note\">\n" . $lng->get ( "uaUnPwRemNote" ) . "</div>\n" );
+	
+	$pge->put ( "<FORM class=\"labsys_mop_std_form\" NAME=\"UnPwRemainder\" METHOD=\"POST\" ACTION=\"#\">\n" . "<input type=\"hidden\" name=\"REDIRECTTO\" value=\"../pages/uaUnPwReminder.php\">\n" . "<fieldset><legend>" . $lng->get ( "eMail" ) . "</legend>\n" . "<div class=\"labsys_mop_in_fieldset\">\n" );
+	
+	$pge->put ( 
+			// email address
+			'<label for="eMail" class="labsys_mop_input_field_label_top">' . $lng->get ( 'eMail' ) . '</label>' . "\n" . '<input tabindex="' . $pge->nextTab ++ . '" type="text" id="eMail" name="EMAIL" class="labsys_mop_input_fullwidth" value="user@server.tld" />' . "\n" );
+	
+	$pge->put ( "</div>\n" . "</fieldset>\n" . "<input tabindex=\"" . $pge->nextTab ++ . "\" type=\"submit\" class=\"labsys_mop_button\">\n" . "</FORM>" );
+	
+	// focus
+	$pge->put ( '<script language="JavaScript" type="text/javascript">
+
                 <!--
-                if (document.UnPwRemainder){
-                  document.UnPwRemainder.eMail.focus();
-                  document.UnPwRemainder.eMail.select();
-                }
-                //-->
-                </script>'
-               );
-  } // /showing reminder stuff
 
+                if (document.UnPwRemainder){
+
+                  document.UnPwRemainder.eMail.focus();
+
+                  document.UnPwRemainder.eMail.select();
+
+                }
+
+                //-->
+
+                </script>' );
+} // /showing reminder stuff
+  
 // show!
-  require( $cfg->get("SystemPageLayoutFile") );   
+require ($cfg->get ( "SystemPageLayoutFile" ));
 ?>
