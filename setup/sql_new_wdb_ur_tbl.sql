b203f53a29bae19dd6ab917d406d1d0f937ead7b
lukas227 <lukas-gh@schwaighofer.name>
2016-08-02 14:02:49 +0200

    correctly add new users

--- setup/sql_new_wdb_ur_tbl.sql
+++ setup/sql_new_wdb_ur_tbl.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS `user_rights` (
-  `idx` bigint(20) NOT NULL,
+  `idx` bigint(20) NOT NULL auto_increment,
   `uid` varchar(32) NOT NULL DEFAULT '',
   `rights` bigint(20) NOT NULL DEFAULT '0' COMMENT 'The rights the user can maximally have.',
   `enabledRights` bigint(20) DEFAULT NULL COMMENT 'The rights the user has currently enabled.',
   `currentTeam` int(9) NOT NULL DEFAULT '0',
-  `history` text NOT NULL
+  `history` text NOT NULL,
+  PRIMARY KEY (`idx`),
+  KEY `uid` (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
