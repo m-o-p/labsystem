4fe24008d411441948855cc35215fa9795199d82
lukas227 <lukas-gh@schwaighofer.name>
2016-08-02 13:07:14 +0200

    fixed indent, removed extra colon

--- setup/sql_new_wdb_ur_tbl.sql
+++ setup/sql_new_wdb_ur_tbl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `user_rights` (
-`idx` bigint(20) NOT NULL,
+  `idx` bigint(20) NOT NULL,
   `uid` varchar(32) NOT NULL DEFAULT '',
-  `rights` bigint(20) NOT NULL DEFAULT '0' COMMENT 'The rights the user can maximally have.',,
+  `rights` bigint(20) NOT NULL DEFAULT '0' COMMENT 'The rights the user can maximally have.',
   `enabledRights` bigint(20) DEFAULT NULL COMMENT 'The rights the user has currently enabled.',
   `currentTeam` int(9) NOT NULL DEFAULT '0',
   `history` text NOT NULL
