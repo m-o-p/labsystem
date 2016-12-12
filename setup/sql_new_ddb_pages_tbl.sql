m-o-p/labsystem

commit 7f888abce260b5d8442f57221b23a1ae7ba1993f

Changed the prototype to have visibility set to 1.

setup/sql_new_ddb_pages_tbl.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `pages` (
 ) ENGINE=MyISAM AUTO_INCREMENT=18 ;
 
 INSERT INTO `pages` (`idx`, `title`, `contents`, `matching_menu`, `visible_for`, `visible_only_in_collection`, `history`) VALUES
-(1, 'prototype page element', '[HTML]\r\n<!-- click on the icon with the eye (second one) on the upper right to see how this page looks like! -->\r\n\r\n<h3>__ELEMENTTITLE__</h3>\r\n\r\n<p>\r\n<b>Hello __FORENAME__ __NAME__</b> (__USERNAME__), at the moment you watch the prototype of a page element!\r\n</p>\r\n\r\n<p>\r\nWhen you create new pages, this template will be used by default.\r\n</p>\r\n<p>\r\nYou can change it as you like, you can even have multiple templates...<br />\r\nThey can be managed <a href="../pages/view.php?address=p2&__LINKQUERY__">here <img src="../syspix/button_manage_elements_13x12.gif" width="13" height="12" border="0"></a>...\r\n</p>\r\n\r\n<p>\r\nTo get back to the edit mode just click <a href="../pages/edit.php?address=p1&__LINKQUERY__"><img src="../syspix/button_edit_13x12.gif" width="13" height="12" border="0"></a> here or on the top right...\r\n</p>', NULL, 0, 1, '2006-03-11 19:10:58: Marc-Oliver Pahl'),
+(1, 'prototype page element', '[HTML]\r\n<!-- click on the icon with the eye (second one) on the upper right to see how this page looks like! -->\r\n\r\n<h3>__ELEMENTTITLE__</h3>\r\n\r\n<p>\r\n<b>Hello __FORENAME__ __NAME__</b> (__USERNAME__), at the moment you watch the prototype of a page element!\r\n</p>\r\n\r\n<p>\r\nWhen you create new pages, this template will be used by default.\r\n</p>\r\n<p>\r\nYou can change it as you like, you can even have multiple templates...<br />\r\nThey can be managed <a href="../pages/view.php?address=p2&__LINKQUERY__">here <img src="../syspix/button_manage_elements_13x12.gif" width="13" height="12" border="0"></a>...\r\n</p>\r\n\r\n<p>\r\nTo get back to the edit mode just click <a href="../pages/edit.php?address=p1&__LINKQUERY__"><img src="../syspix/button_edit_13x12.gif" width="13" height="12" border="0"></a> here or on the top right...\r\n</p>', 1, 0, 1, '2006-03-11 19:10:58: Marc-Oliver Pahl'),
 (2, 'Managing the elements...', '[HTML]\r\n<h3><img src="../syspix/button_manage_elements_13x12.gif" width="13" height="12" border="0"> __ELEMENTTITLE__</h3>\r\n\r\n<table align="center" cellspacing="10px" style="width:600px; table-layout:fixed; ">\r\n<tr><td></td><td width="50px;"><td></td></tr><!-- table-layout fixed => sizes set to first (this) row -->\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=p1&saveAsNew&__LINKQUERY__"><b>new page</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_p_row">\r\n    <img src="../syspix/button_new_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=p&__LINKQUERY__"><b>manage pages</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=m1&saveAsNew&__LINKQUERY__"><b>new multiple choice</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_m_row">\r\n    <img src="../syspix/button_new_multichoice_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=m&__LINKQUERY__"><b>manage multiple choices</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=i1&saveAsNew&__LINKQUERY__"><b>new input</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_i_row">\r\n    <img src="../syspix/button_new_input_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=i&__LINKQUERY__"><b>manage inputs</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=c1&saveAsNew&__LINKQUERY__"><b>new collection</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_c_row">\r\n    <img src="../syspix/button_new_collection_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=c&__LINKQUERY__"><b>manage collections</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=l1&saveAsNew&__LINKQUERY__"><b>new lab</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_l_row">\r\n    <img src="../syspix/button_new_lab_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=l&__LINKQUERY__"><b>manage labs</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=s1&saveAsNew&__LINKQUERY__"><b>new schedule</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_s_row">\r\n    <img src="../syspix/button_new_schedule_36x36.gif" width="36" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=s&__LINKQUERY__"><b>manage schedules</b>...</a>\r\n  </td>\r\n </tr>\r\n</table>\r\n\r\n<div class="labsys_mop_note">\r\n<div class="labsys_mop_h3">This page is dynamic!</div>\r\n<p>The big advantage is that you can easily add your own templates (<a href="../pages/edit.php?address=p2&__LINKQUERY__"><img src="../syspix/button_edit_13x12.gif" width="13" height="12" border="0"></a>)!</p>\r\n<ul>\r\n  <li>create your template</li>\r\n  <li>save it</li>\r\n  <li>copy the link (<img src="../syspix/button_link2_13x12.gif" width="13" height="12" border="0">)</li>\r\n  <li>add it to your section here</li>\r\n  <li>replace "__LINKQUERY__" by "saveAsNew&__LINKQUERY&#95;&#95;" (the save as new checkbox will be enabled then)</li>\r\n  <li>done</li>\r\n</ul>\r\n</div>', 'elements', 4, 0, '2006-03-11 19:10:58: Marc-Oliver Pahl'),
 /* (3, will be inserted by the script according to config... */
 (4, 'So how does it work?', '[HTML]\r\n<!-- click on the icon with the eye (second one) on the upper right to see how this page looks like! -->\r\n\r\n<h3>__ELEMENTTITLE__</h3>\r\n\r\n<p>\r\n<b>Hello __FORENAME__ __NAME__</b> (__USERNAME__), this lab is just for getting familiar with the lab system!\r\n</p>\r\n\r\n<p>\r\nYou find all elements you''ll find in a &quot;real&quot; lab here to get familar with them.\r\n</p>\r\n\r\n<p>\r\nIn a &quot;real&quot; prelab you will find many information that will help you (and is necessary) during the lab.\r\n</p>\r\n\r\n<p>All labs have two parts:\r\n<ul>\r\n<li>The prelab</li>\r\n<li>The lab</li>\r\n</ul>\r\nThe <b>prelab</b> should give you the <b>theoretical background</b> of what you will do later on in the lab session. The more careful you read the texts, the easier the lab will be for you.<br />\r\nThere are multiple choice questions after most of the prelab sections. These questions should help you to recover and memorize the most important aspects of the topics you have just read about.</p>\r\n\r\n<p>\r\nYou won''t have to wait until someone corrects your multiple choice questions. On the page with all prelab questions (last menu entry of the prelab) you find a button "<b>check prelab</b>". Clicking it makes the system check your answers. You''ll see immediately what was wrong and what was right and sometimes you will also get some information why that is the answer.<br />\r\nYou have multiple attempts to answer the questions. By default you have three (3) attempts (that value can be changed in the config file).\r\n</p>\r\n\r\n<p>To be able to perform the lab, <b>each groupmember</b> has to have answered <b>all prelab-questions</b>. When all your teammates are finished with the prelab session, the lab session will become visible to you.</p>\r\n\r\n<p>In the <b>lab</b> you will use the techniques you read about in the prelab. You solve some exercises together as a team. The available lab text gives you some instructions. During the lab you''ll  have to <b>answer some questions</b>. You do this as a team (as you will see all of you have the same answer fields). You should switch formulating the answers so that each groupmember writes down some of the answers. It is also a challenge to formulate one''s thoughts clearly...</p>', '', 1, 1, '2006-03-11 19:10:58: Marc-Oliver Pahl'),

commit 750b5e0631542afdec3e70234512c6aa605c762b

Prototype should have visibility 1
This Fixes commit 7f888abce260b5d8442f57221b23a1ae7ba1993f which
accidentally changed matching_menu instead of visibility

setup/sql_new_ddb_pages_tbl.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `pages` (
 ) ENGINE=MyISAM AUTO_INCREMENT=18 ;
 
 INSERT INTO `pages` (`idx`, `title`, `contents`, `matching_menu`, `visible_for`, `visible_only_in_collection`, `history`) VALUES
-(1, 'prototype page element', '[HTML]\r\n<!-- click on the icon with the eye (second one) on the upper right to see how this page looks like! -->\r\n\r\n<h3>__ELEMENTTITLE__</h3>\r\n\r\n<p>\r\n<b>Hello __FORENAME__ __NAME__</b> (__USERNAME__), at the moment you watch the prototype of a page element!\r\n</p>\r\n\r\n<p>\r\nWhen you create new pages, this template will be used by default.\r\n</p>\r\n<p>\r\nYou can change it as you like, you can even have multiple templates...<br />\r\nThey can be managed <a href="../pages/view.php?address=p2&__LINKQUERY__">here <img src="../syspix/button_manage_elements_13x12.gif" width="13" height="12" border="0"></a>...\r\n</p>\r\n\r\n<p>\r\nTo get back to the edit mode just click <a href="../pages/edit.php?address=p1&__LINKQUERY__"><img src="../syspix/button_edit_13x12.gif" width="13" height="12" border="0"></a> here or on the top right...\r\n</p>', 1, 0, 1, '2006-03-11 19:10:58: Marc-Oliver Pahl'),
+(1, 'prototype page element', '[HTML]\r\n<!-- click on the icon with the eye (second one) on the upper right to see how this page looks like! -->\r\n\r\n<h3>__ELEMENTTITLE__</h3>\r\n\r\n<p>\r\n<b>Hello __FORENAME__ __NAME__</b> (__USERNAME__), at the moment you watch the prototype of a page element!\r\n</p>\r\n\r\n<p>\r\nWhen you create new pages, this template will be used by default.\r\n</p>\r\n<p>\r\nYou can change it as you like, you can even have multiple templates...<br />\r\nThey can be managed <a href="../pages/view.php?address=p2&__LINKQUERY__">here <img src="../syspix/button_manage_elements_13x12.gif" width="13" height="12" border="0"></a>...\r\n</p>\r\n\r\n<p>\r\nTo get back to the edit mode just click <a href="../pages/edit.php?address=p1&__LINKQUERY__"><img src="../syspix/button_edit_13x12.gif" width="13" height="12" border="0"></a> here or on the top right...\r\n</p>', '', 1, 1, '2006-03-11 19:10:58: Marc-Oliver Pahl'),
 (2, 'Managing the elements...', '[HTML]\r\n<h3><img src="../syspix/button_manage_elements_13x12.gif" width="13" height="12" border="0"> __ELEMENTTITLE__</h3>\r\n\r\n<table align="center" cellspacing="10px" style="width:600px; table-layout:fixed; ">\r\n<tr><td></td><td width="50px;"><td></td></tr><!-- table-layout fixed => sizes set to first (this) row -->\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=p1&saveAsNew&__LINKQUERY__"><b>new page</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_p_row">\r\n    <img src="../syspix/button_new_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=p&__LINKQUERY__"><b>manage pages</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=m1&saveAsNew&__LINKQUERY__"><b>new multiple choice</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_m_row">\r\n    <img src="../syspix/button_new_multichoice_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=m&__LINKQUERY__"><b>manage multiple choices</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=i1&saveAsNew&__LINKQUERY__"><b>new input</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_i_row">\r\n    <img src="../syspix/button_new_input_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=i&__LINKQUERY__"><b>manage inputs</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=c1&saveAsNew&__LINKQUERY__"><b>new collection</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_c_row">\r\n    <img src="../syspix/button_new_collection_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=c&__LINKQUERY__"><b>manage collections</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=l1&saveAsNew&__LINKQUERY__"><b>new lab</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_l_row">\r\n    <img src="../syspix/button_new_lab_39x36.gif" width="39" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=l&__LINKQUERY__"><b>manage labs</b>...</a>\r\n  </td>\r\n </tr>\r\n\r\n <tr>\r\n  <td align="right">\r\n    <a href="../pages/edit.php?address=s1&saveAsNew&__LINKQUERY__"><b>new schedule</b>...</a>\r\n  </td>\r\n  <td class="labsys_mop_s_row">\r\n    <img src="../syspix/button_new_schedule_36x36.gif" width="36" height="36" border="0">\r\n  </td>\r\n  <td align="left">\r\n    <a href="../pages/manage.php?address=s&__LINKQUERY__"><b>manage schedules</b>...</a>\r\n  </td>\r\n </tr>\r\n</table>\r\n\r\n<div class="labsys_mop_note">\r\n<div class="labsys_mop_h3">This page is dynamic!</div>\r\n<p>The big advantage is that you can easily add your own templates (<a href="../pages/edit.php?address=p2&__LINKQUERY__"><img src="../syspix/button_edit_13x12.gif" width="13" height="12" border="0"></a>)!</p>\r\n<ul>\r\n  <li>create your template</li>\r\n  <li>save it</li>\r\n  <li>copy the link (<img src="../syspix/button_link2_13x12.gif" width="13" height="12" border="0">)</li>\r\n  <li>add it to your section here</li>\r\n  <li>replace "__LINKQUERY__" by "saveAsNew&__LINKQUERY&#95;&#95;" (the save as new checkbox will be enabled then)</li>\r\n  <li>done</li>\r\n</ul>\r\n</div>', 'elements', 4, 0, '2006-03-11 19:10:58: Marc-Oliver Pahl'),
 /* (3, will be inserted by the script according to config... */
 (4, 'So how does it work?', '[HTML]\r\n<!-- click on the icon with the eye (second one) on the upper right to see how this page looks like! -->\r\n\r\n<h3>__ELEMENTTITLE__</h3>\r\n\r\n<p>\r\n<b>Hello __FORENAME__ __NAME__</b> (__USERNAME__), this lab is just for getting familiar with the lab system!\r\n</p>\r\n\r\n<p>\r\nYou find all elements you''ll find in a &quot;real&quot; lab here to get familar with them.\r\n</p>\r\n\r\n<p>\r\nIn a &quot;real&quot; prelab you will find many information that will help you (and is necessary) during the lab.\r\n</p>\r\n\r\n<p>All labs have two parts:\r\n<ul>\r\n<li>The prelab</li>\r\n<li>The lab</li>\r\n</ul>\r\nThe <b>prelab</b> should give you the <b>theoretical background</b> of what you will do later on in the lab session. The more careful you read the texts, the easier the lab will be for you.<br />\r\nThere are multiple choice questions after most of the prelab sections. These questions should help you to recover and memorize the most important aspects of the topics you have just read about.</p>\r\n\r\n<p>\r\nYou won''t have to wait until someone corrects your multiple choice questions. On the page with all prelab questions (last menu entry of the prelab) you find a button "<b>check prelab</b>". Clicking it makes the system check your answers. You''ll see immediately what was wrong and what was right and sometimes you will also get some information why that is the answer.<br />\r\nYou have multiple attempts to answer the questions. By default you have three (3) attempts (that value can be changed in the config file).\r\n</p>\r\n\r\n<p>To be able to perform the lab, <b>each groupmember</b> has to have answered <b>all prelab-questions</b>. When all your teammates are finished with the prelab session, the lab session will become visible to you.</p>\r\n\r\n<p>In the <b>lab</b> you will use the techniques you read about in the prelab. You solve some exercises together as a team. The available lab text gives you some instructions. During the lab you''ll  have to <b>answer some questions</b>. You do this as a team (as you will see all of you have the same answer fields). You should switch formulating the answers so that each groupmember writes down some of the answers. It is also a challenge to formulate one''s thoughts clearly...</p>', '', 1, 1, '2006-03-11 19:10:58: Marc-Oliver Pahl'),