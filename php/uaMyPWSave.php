m-o-p/labsystem

commit 9737d9e090aed72a803bc782bdec6aaef4cda286

Fixed that the right text is shown when the admin user is created. Escape PW in ua PW save now. Removed session directory since the default from PHP is used.

php/uaMyPWSave.php
@@ -61,7 +61,7 @@ else{ // save new PW
                                 $cfg->get('UserDatabasePassWord'), 
                                 $cfg->get('UserDatabaseName'));
     $accordingUID = ( $usr->isOfKind( IS_DB_USER_ADMIN ) && $usr->isSeeingSomeonesData() ? $usr->theSeeingUid()  : $usr->uid  );                
-    $userDBC->mkUpdate( $cfg->get('UserDBField_password')."='".password_hash( $_POST['NEWPW'], PASSWORD_DEFAULT )."'",
+    $userDBC->mkUpdate( $cfg->get('UserDBField_password')."='".$usrDB->escapeString( password_hash( $_POST['NEWPW'], PASSWORD_DEFAULT ) )."'",
                         $cfg->get('UserDatabaseTable'), 
                         $cfg->get('UserDBField_uid')."='".$accordingUID."'"
                        );

sessiondata/.htaccess
@@ -1,2 +1,0 @@
-Order Deny,Allow
-Deny from all

sessiondata/index.html
@@ -1,43 +1,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
-<html>
-<!--
-    labsystem.m-o-p.de - 
-                    the web based eLearning tool for practical exercises
-    Copyright (C) 2009  Marc-Oliver Pahl
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
-    ----------------------------------------------------------------------
-    
-    This is just an empty file to prevent from file listing when .htaccess
-    is disabled and the webserver has Indexin enabled.
-    
-    ----------------------------------------------------------------------
--->
-<head><title>labsystem.m-o-p.de</title></head>
-<body>
-  <h1>Danger!</h1>
-  <p>
-    If you see this page your session data can possibly be read from the
-    Internet.
-  </p>
-  <h4>
-    Protect this directory from being served by your webserver!
-  </h4>
-  <p>
-    You can do this by enabling .htaccess overriding of access control
-    or setting the deny policy for the directory directly in your webserver.
-  </p>
-</body>
-</html>

setup/index.php
@@ -249,9 +249,9 @@ if(!$usrDB->table_exists( $cfg->get("UserDatabaseTable") ))
                    )';
          $ret = $usrDB->query($query);
          if ( $ret !== false )
-             say_failed($usrDB);
-         else
              echo("<h4><b>admin user \"".$cfg->get("RightsAdminUsername")."\" with password \"admin\" created!</b> (you need this to log on the system. Change the password!!!!!!)</h4><br>\n");
+         else
+             say_failed($usrDB);
        }
 } else say_skipped();
 

commit 9f7822848fdcd80752b1bb578d802fcaf7245ffc

bugfix: call escapeString on correct class object
fixes regression introduced by 9737d9e090aed72a803bc782bdec6aaef4cda286
which prevented users from changing their password

php/uaMyPWSave.php
@@ -61,7 +61,7 @@ else{ // save new PW
                                 $cfg->get('UserDatabasePassWord'), 
                                 $cfg->get('UserDatabaseName'));
     $accordingUID = ( $usr->isOfKind( IS_DB_USER_ADMIN ) && $usr->isSeeingSomeonesData() ? $usr->theSeeingUid()  : $usr->uid  );                
-    $userDBC->mkUpdate( $cfg->get('UserDBField_password')."='".$usrDB->escapeString( password_hash( $_POST['NEWPW'], PASSWORD_DEFAULT ) )."'",
+    $userDBC->mkUpdate( $cfg->get('UserDBField_password')."='".$userDBC->escapeString( password_hash( $_POST['NEWPW'], PASSWORD_DEFAULT ) )."'",
                         $cfg->get('UserDatabaseTable'), 
                         $cfg->get('UserDBField_uid')."='".$accordingUID."'"
                        );