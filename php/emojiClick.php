0de266b0ece942984dfc79111bb151457615c736
Marc-Oliver Pahl <pahl@tum.de>
2019-08-07 18:43:02 +0200

    Did some comments.

--- php/emojiClick.php
+++ php/emojiClick.php
@@ -2,24 +2,31 @@
 /** This file handles the XMLHttpRequest of the insertEmojiSelection() function in scripts.js*/
 
 require_once ("../include/init.inc");
+
+if (! ($usr->isOfKind ( IS_USER )) /* valid call? */
+    ){
+        trigger_error ( $lng->get ( "NotAllowedToMkCall" ), E_USER_ERROR );
+        exit ();
+}
+
 global $eDBI;
 
 if (!empty($_REQUEST['elemId'])) {
     $elemId = $_REQUEST['elemId'];
 } else {
-    echo "Wrong element ID!";
+    echo "Missing element ID!"; // TODO: use language strings
 }
 
 if (!empty($_REQUEST['uid'])) {
     $uid = $_REQUEST['uid'];
 } else {
-    echo "Wrong user ID!";
+    echo "Missing user ID!";
 }
 
 if (!empty($_REQUEST['emojiId'])) {
     $emojiId = $_REQUEST['emojiId'];
 } else {
-    echo "Wrong emoji ID!";
+    echo "Missing emoji ID!";
 }
 
 //$emojiDBI = new EmojiDBInterface();
@@ -30,3 +37,4 @@ $eDBI->insertRow($_REQUEST['elemId'], $_REQUEST['uid'], $_REQUEST['emojiId']);
 
 echo "OK";
 
+?>
--- php/emojiUpdate.php
+++ php/emojiUpdate.php
@@ -2,17 +2,24 @@
 /** This file handles the XMLHttpRequest of the updateEmojiMenu() function in scripts.js */
 
 require_once ("../include/init.inc");
+
+if (! ($usr->isOfKind ( IS_USER )) /* valid call? */
+    ){
+        trigger_error ( $lng->get ( "NotAllowedToMkCall" ), E_USER_ERROR );
+        exit ();
+}
+
 global $eDBI;
 
 if (!empty($_REQUEST['elemId'])) {
     $elemId = $_REQUEST['elemId'];
 } else {
-    echo "Wrong element ID!";
+    echo "Missing element ID!"; // TODO: use language strings
 }
 
 $selections = $eDBI->findNumOfSelectionsFor(1, $elemId);
 $elem=$selections->fetch_array();
-$data["like"] = $elem[0];
+$data['like'] = $elem[0];
 
 $selections = $eDBI->findNumOfSelectionsFor(2, $elemId);
 $elem=$selections->fetch_array();
@@ -28,3 +35,5 @@ $data["dislike"] =  $elem[0];
 
 header('Content-type:application/json');
 echo json_encode( $data );
+
+?>
