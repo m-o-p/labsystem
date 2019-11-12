f11c19c2139dc09f28f51003d90056b1eb38e782
Christoph Schwarzenberg <c.schwarzenberg@tum.de>
2019-11-12 21:13:49 +0100

    Migrate myRights page to Twig

--- pages/myRights.php
+++ pages/myRights.php
@@ -19,68 +19,59 @@
  */
 
 /**
-* This page is the interface to restrict one's rights temporarily.
-* This makes sense to disable some edit functionality (and buttons)...
-*
-* @module     ../pages/myRights.php
-* @author     Marc-Oliver Pahl
-* @copyright  Marc-Oliver Pahl 2005
-* @version    1.0
-*
-* HTML W3 VALIDATED!
-*/
-require( "../include/init.inc" );
+ * This page is the interface to restrict one's rights temporarily.
+ * This makes sense to disable some edit functionality (and buttons)...
+ *
+ * @module     ../pages/myRights.php
+ * @author     Marc-Oliver Pahl
+ * @copyright  Marc-Oliver Pahl 2005
+ * @version    1.0
+ *
+ * HTML W3 VALIDATED!
+ */
+require("../include/init.inc");
+require(INCLUDE_DIR . "/twig.inc");
 
-$pge->title        = $lng->get("titleMyRightsPge");
+
+$pge->title = $lng->get("titleMyRightsPge");
 $pge->matchingMenu = $lng->get("MnuEntryMyRights");
-$pge->visibleFor   = IS_USER;
-
-if ( !$pge->isVisible() ){ // directly show warning and close.
-  require( $cfg->get("SystemPageLayoutFile") );
-  exit;
-}
-
-// title
-  $pge->put( "<div class=\"labsys_mop_h2\">__PAGETITLE__</div>\n" );
-
-// note
-  if ( $lng->get("myRightsNote") != "" ) $pge->put( "<div class=\"labsys_mop_note\">\n".$lng->get("myRightsNote")."</div>\n" );
+$pge->visibleFor = IS_USER;
 
 $notRemovableRights = intval(IS_USER);
 $newUsrRights = $notRemovableRights;
 
-$pge->put( '<FORM class="labsys_mop_std_form" NAME="myRightsEdit" METHOD="POST" ACTION="#">'.PHP_EOL.
-           "<fieldset><legend>".$lng->get("rights").' ('.$usr->userRights.'/'.$usr->userMaximumRights.')</legend>'.PHP_EOL.
-           "<div class=\"labsys_mop_in_fieldset\">\n" );
-// user's possible rights
-  for ($i=1; $i<=MAX_USER_ROLE; $i=$i<<1){
-    if ( $usr->isOfKind( $i, $usr->userMaximumRights ) ){ // does the user have this right?
-    	if (isset($_POST['UR_'.$i]) && $_POST['UR_'.$i]==$i ){
-    		$newUsrRights |= $i;
-    	}
-      $pge->put( rightsBox( "UR_".$i, $i, (!empty($_POST) ? $newUsrRights : $usr->userRights), ($i & $notRemovableRights == $i) )."<label for=\"UR_".$i."\" class=\"labsys_mop_input_field_label\">".$lng->get("Explain_UR_".$i)." ($i)</label><br>\n" );
+
+$rights_choices = [];
+for ($i = 1; $i <= MAX_USER_ROLE; $i = $i << 1) {
+  array_push($rights_choices, $i);
+
+  // does the user have this right?
+  if ($usr->isOfKind($i, $usr->userMaximumRights)) {
+    if (isset($_POST['UR_' . $i]) && $_POST['UR_' . $i] == $i) {
+      $newUsrRights |= $i;
     }
   }
-  if (!empty($_POST) && ($newUsrRights != $usr->userRights)){
-  	if (!isset($_POST['thisTabOnly'])){
-  		$usr->saveCurrentRights($newUsrRights);
-  		makeLogEntry( 'system', 'user rights changed to '.$newUsrRights.'/'.$usr->userMaximumRights );
-  		$url->rem('myRights');
-  	}else{
-  		//only in this tab...
-  		$url->put('myRights', $newUsrRights);
-  		$usr->userRights=$newUsrRights; // validity checked above already!
-  	}
+}
+
+if (!empty($_POST) && ($newUsrRights != $usr->userRights)) {
+  if (!isset($_POST['thisTabOnly'])) {
+    $usr->saveCurrentRights($newUsrRights);
+    makeLogEntry('system', 'user rights changed to ' . $newUsrRights . '/' . $usr->userMaximumRights);
+    $url->rem('myRights');
+  } else {
+    //only in this tab...
+    var_dump($newUsrRights);
+    $url->put('myRights', $newUsrRights);
+    // validity checked above already!
+    $usr->userRights = $newUsrRights;
   }
+}
 
-$pge->put( "</div>\n".
-           "</fieldset>\n".
-           "<input tabindex=\"".++$pge->nextTab."\" type=\"submit\" class=\"labsys_mop_button\" name=\"change_rights\" value=\"".$lng->get("apply")."\" onclick='isDirty=false'>".PHP_EOL.
-		   '<input type="checkbox" id="thisTabOnly" name="thisTabOnly" value="1" tabindex="'.$pge->nextTab++.'"'.( isset($_POST['thisTabOnly'])?' checked="checked"':'').' onchange="isDirty=true"/>'.PHP_EOL.
-		   '<label for="thisTabOnly" class="labsys_mop_input_field_label">'.infoArrow( $lng->get('changeRightsInThisTabOnly'), false ).' '.$lng->get('changeRightsInThisTabOnly').'</label>'.PHP_EOL.
-		   "</FORM>"
-          );
+$pge->add_context('rights_choices', $rights_choices);
+$pge->add_context('rights', (!empty($_POST) ? $newUsrRights : $usr->userRights));
+$pge->add_context('nonRemovableRights', [$notRemovableRights,]);
+$pge->add_context('thisTabOnly', isset($_POST['thisTabOnly']));
 
-// show!
-  require( $cfg->get("SystemPageLayoutFile") );
-?>
+$pge->set_template('pages/myRights.html.twig');
+
+require($cfg->get("SystemPageLayoutFile"));
