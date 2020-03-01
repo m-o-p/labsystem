265d1b873ca8325e03810840ae67b3f74e6da1f8
Christoph Schwarzenberg <c.schwarzenberg@tum.de>
2020-03-01 14:53:11 +0100

    Migrate to Markdown installed via composer

--- plugins/phpMarkDown/install.sh
+++ plugins/phpMarkDown/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-fileName="php-markdown-extra-1.2.6.zip"
-wget http://littoral.michelf.ca/code/php-markdown/$fileName
-unzip -jn $fileName
-rm $fileName
+echo "This dependency is installed by Composer"
+
+#fileName="php-markdown-extra-1.2.6.zip"
+#wget http://littoral.michelf.ca/code/php-markdown/$fileName
+#unzip -jn $fileName
+#rm $fileName
